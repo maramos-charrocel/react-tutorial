a99b8c02e38b97d2b216af7344271eb0a0538ebb
maramos-charrocel <maramos@charrocel.com>
2024-05-07 00:12:40 -0600

    upd: reder FilterableTable instead of App

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+//import App from './App.tsx'
+//import './index.css'
+import { FilterableTable } from './learning-tutorial/Filterable-table.tsx'
+import {PRODUCTS} from './learning-tutorial/products'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <FilterableTable products={PRODUCTS}/>
   </React.StrictMode>,
 )
