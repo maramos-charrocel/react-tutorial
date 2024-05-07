3762a38d66b5b1ac42862973c2dde75fdac1b51d
maramos-charrocel <maramos@charrocel.com>
2024-05-06 23:07:32 -0600

    fix: import Counter from /Counter.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {Counter} from './counter.tsx'
+import { Counter } from './Counter.tsx'
 
 
 function App() {
