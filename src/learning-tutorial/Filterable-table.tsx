7722f43ccf108dbb1fb41c5827254d739828687e
maramos-charrocel <maramos@charrocel.com>
2024-05-07 23:38:09 -0600

    refactor: create table component

--- src/learning-tutorial/Filterable-table.tsx
+++ src/learning-tutorial/Filterable-table.tsx
@@ -1,13 +1,37 @@
 import { IProduct } from "./products"
 import { isArray } from "../helpers/type-validation";
+import { ReactNode } from "react";
+import {Table} from "./Table.tsx";
 
 export interface IFilterableTableProps{
-    products: IProduct[]
+    products: IProduct[];
 }
 
 export function FilterableTable({products}: IFilterableTableProps) {
     if(!isArray(products)){
         return <div>Products must be an array</div>
     }
-    return <div>Filterable Table</div>
+    const rows: ReactNode[] =[]
+
+    products.forEach((product) => {
+        rows.push(
+            <tr key={product.name}>
+                <td>{product.name}</td>
+                <td>{product.price}</td>
+            </tr>
+        )
+    })
+    return <div>
+        <form>
+            <label>
+                Search...
+                <input type="search"/>
+            </label>
+            <label>
+            <input type="checkbox" />
+                Only products in stock
+            </label>
+        </form>
+        <Table products={products} />
+    </div>
 }
