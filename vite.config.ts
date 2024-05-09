96a1212f207f78986f56c6e17e84c7c0622ac50f
maramos-charrocel <maramos@charrocel.com>
2024-05-08 21:52:39 -0600

    chore: install and configure test dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+///<reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test:{
+    //habilita vitest en todos los archivos de test sin necesidad de importar vitest
+    globals: true,
+    //Le dice a vitest que ambiente y archivos se va a usar para el test y extensiones utilizar
+    include: ['src/**/*.{test.spec}.{js,ts,jsx,tsx}'],
+    //para que @testing-library pueda montar los componentes de react en un dom (sin navegador) y testearlos
+    environment: 'jsdom',
+  },
+});
